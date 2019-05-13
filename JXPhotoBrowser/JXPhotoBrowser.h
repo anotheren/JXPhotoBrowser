0d62bdc561a1e325f1d97d16234a5ab465fdb680
liudong <liudong.edward@gmail.com>
2019-05-13 16:41:43 +0800

    添加 Carthage 支持

---
+++ JXPhotoBrowser/JXPhotoBrowser.h
@@ -0,0 +1,19 @@
+//
+//  JXPhotoBrowser.h
+//  JXPhotoBrowser
+//
+//  Created by 刘栋 on 2019/5/13.
+//  Copyright © 2019 anotheren.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JXPhotoBrowser.
+FOUNDATION_EXPORT double JXPhotoBrowserVersionNumber;
+
+//! Project version string for JXPhotoBrowser.
+FOUNDATION_EXPORT const unsigned char JXPhotoBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JXPhotoBrowser/PublicHeader.h>
+
+
